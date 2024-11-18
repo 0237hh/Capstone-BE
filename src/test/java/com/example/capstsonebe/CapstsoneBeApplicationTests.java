8762b64419ca8dd0d27407a1801967c3ef2a6c7b
0237hh <153145663+0237hh@users.noreply.github.com>
2024-11-18 12:11:44 +0900

    first commit

---
+++ src/test/java/com/example/capstsonebe/CapstsoneBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.capstsonebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstsoneBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
